ef56b37310e4771354acd81686be30444f8e9b5c
TheVeryStarlk <45287860+TheVeryStarlk@users.noreply.github.com>
2026-02-15 00:33:10 +0300

    Remove exception catches

--- BetaSharp/NbtIo.cs
+++ BetaSharp/NbtIo.cs
@@ -15,37 +15,13 @@ public static class NbtIo
     public static void WriteCompressed(NBTTagCompound tag, OutputStream output)
     {
         var stream = new DataOutputStream(new GZIPOutputStream(output));
-
-        try
-        {
-            Write(tag, stream);
-        }
-        catch (Exception exception)
-        {
-            Console.WriteLine($"Failed to write a compressed NBT; {exception.Message}");
-        }
-        finally
-        {
-            stream.close();
-        }
+        Write(tag, stream);
     }
 
     public static NBTTagCompound ReadCompressed(InputStream input)
     {
         var stream = new DataInputStream(new GZIPInputStream(input));
-
-        NBTTagCompound tag;
-
-        try
-        {
-            tag = Read((DataInput) stream);
-        }
-        finally
-        {
-            stream.close();
-        }
-
-        return tag;
+        return Read(stream);
     }
 
     public static NBTTagCompound Read(DataInput input)
